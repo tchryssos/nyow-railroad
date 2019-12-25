6ef3e687718146f20e8aa3120aa220ec24d1b9f1
Troy Chryssos <troychryssos@gmail.com>
2019-12-25 10:27:21 -0500

    initial commit, trying parcel

---
+++ js/threejsSetup.js
@@ -0,0 +1,21 @@
+// Scene and camera
+export const scene = new THREE.Scene()
+export const camera = new THREE.PerspectiveCamera(
+	75, window.innerWidth / window.innerHeight, 0.1, 1000,
+)
+export const renderer = new THREE.WebGLRenderer()
+renderer.setSize( window.innerWidth, window.innerHeight)
+document.body.appendChild( renderer.domElement)
+
+// Objects
+const geometry = new THREE.BoxGeometry( 1, 1, 1 )
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+const cube = new THREE.Mesh( geometry, material )
+scene.add( cube )
+
+camera.position.z = 5
+
+export const animate = () => {
+	requestAnimationFrame( animate )
+	renderer.render( scene, camera )
+}
