b547692bdbcc076020d2a694f29ef21cc482efe7
Troy Chryssos <troychryssos@gmail.com>
2019-12-25 13:18:15 -0500

    three imports for treeshaking

--- js/threejs.js
+++ js/threejs.js
@@ -1,17 +1,21 @@
 import * as THREE from 'three'
+import {
+	Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial,
+	Mesh,
+} from 'three'
 import times from 'ramda/src/times'
 import { green3, randomGreen } from './colors'
 import { randomOrNegativeNumber, randomNumber } from './util'
 
 //// SETUP - START ////
 // Scene and camera
-export const scene = new THREE.Scene()
-export const camera = new THREE.PerspectiveCamera(
+export const scene = new Scene()
+export const camera = new PerspectiveCamera(
 	75, window.innerWidth / window.innerHeight, 0.1, 1000,
 )
-camera.position.z = 150
-camera.position.y = 3
-export const renderer = new THREE.WebGLRenderer()
+camera.position.z = 200
+camera.position.y = 5
+export const renderer = new WebGLRenderer()
 
 // Setup renderer
 renderer.setSize( window.innerWidth, window.innerHeight)
@@ -24,12 +28,12 @@ document.body.appendChild(renderer.domElement)
 // Adding to scenes
 export const addCube = ({
 	posX = 0, posY = 0, posZ = 0,
-	objX = 1, objY = 1, objZ = 1,
-	objColor = green3,
+	objX = 0.5, objY = 0.5, objZ = 0.5,
+	objColor = randomGreen(),
 }) => {
-	const geometry = new THREE.BoxGeometry(objX, objY, objZ)
-	const material = new THREE.MeshBasicMaterial({ color: objColor })
-	const cube = new THREE.Mesh(geometry, material)
+	const geometry = new BoxGeometry(objX, objY, objZ)
+	const material = new MeshBasicMaterial({ color: objColor })
+	const cube = new Mesh(geometry, material)
 	cube.position.set(posX, posY, posZ)
 	scene.add(cube)
 }
@@ -39,7 +43,6 @@ export const generateNObjects = (n, generatorFunc) => {
 		generatorFunc({
 			posX: randomOrNegativeNumber(nthTime),
 			posZ: randomNumber(nthTime),
-			objColor: randomGreen(),
 		})
 	}
 	times(generator, n)
